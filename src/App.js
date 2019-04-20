669d6f4ff679cd7d38b708776b2a2c0b0f0b5add
Kevin <kevin.freistroffer@gmail.com>
2019-04-19 20:32:11 -0700

    zen checkboxes css imported

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '@zendeskgarden/react-textfields/dist/styles.css';
+import '@zendeskgarden/react-checkboxes/dist/styles.css';
 import { ThemeProvider } from '@zendeskgarden/react-theming';
 import MyForm from './MyForm';
 
